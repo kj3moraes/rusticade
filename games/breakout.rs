4f5bf5bb537cb0f3508ac6200c7eba719debbdde
Keane Moraes <lordvader3002@gmail.com>
2023-06-10 20:25:41 -0400

    initial code for the new games

---
+++ games/breakout.rs
@@ -0,0 +1,9 @@
+use ruscii::app::{App, State};
+use ruscii::drawing::Pencil;
+use ruscii::gui::FPSCounter;
+use ruscii::keyboard::{Key, KeyEvent};
+use ruscii::spatial::Vec2;
+use ruscii::terminal::{Color, Style, Window};
+
+use rand::{self, prelude::*};
+
